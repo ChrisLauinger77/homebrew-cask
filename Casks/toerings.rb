2c2ab9549101628e2c1fc8d9e6f4876a33a18b83
Christian Lauinger <christian.lauinger@gmail.com>
2026-07-19 11:26:50 +0200

    Centralize Homebrew cask updates

---
+++ Casks/toerings.rb
@@ -0,0 +1,21 @@
+cask "toerings" do
+  version "0.0.1"
+  sha256 "21d37eeb56b1b928dbd0851127f0ac3ff6e3f40f3d53511d313286e0d647b5c1"
+
+  url "https://github.com/ChrisLauinger77/toerings/releases/download/v#{version}/ToeRings_#{version}_universal.app.zip"
+  name "ToeRings"
+  desc "Themeable desktop system monitor"
+  homepage "https://github.com/ChrisLauinger77/toerings"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "ToeRings.app"
+
+  caveats <<~EOS
+    #{token} is ad-hoc signed but not notarized. On first launch, Control-click the app
+    in Finder, choose 'Open', and confirm the security dialog.
+  EOS
+end
